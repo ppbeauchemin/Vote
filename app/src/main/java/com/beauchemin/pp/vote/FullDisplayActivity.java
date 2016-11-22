75e4977231668cfb8434021e4c641132222883b8
PP <pp@MacBook-PP.local>
2016-11-22 09:51:16 +0100

    remove title bar from fullscreen image view

--- app/src/main/java/com/beauchemin/pp/vote/FullDisplayActivity.java
+++ app/src/main/java/com/beauchemin/pp/vote/FullDisplayActivity.java
@@ -2,6 +2,8 @@ package com.beauchemin.pp.vote;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import butterknife.BindView;
@@ -14,6 +16,9 @@ public class FullDisplayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_full_display);
         ButterKnife.bind(this);
         full.setImageResource(getIntent().getIntExtra("image", R.mipmap.ic_launcher));
